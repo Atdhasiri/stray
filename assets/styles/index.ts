f36684adb7f621a0afa09082a72e94cde0d6d4a5
focusza1999 <60870065+focusza1999@users.noreply.github.com>
2021-11-23 16:41:07 +0700

    filter now pressable

--- assets/styles/index.ts
+++ assets/styles/index.ts
@@ -299,4 +299,47 @@ export default StyleSheet.create({
   iconMenu: {
     alignItems: "center",
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    marginTop: 5,
+    backgroundColor: "#2196F3",
+  },
+  buttonDangerClose: {
+    marginTop: 5,
+    backgroundColor: "#d2691e",
+  },
+
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
+
 });
