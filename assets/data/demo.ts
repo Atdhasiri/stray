d71c112ea0a100b6a5f0f10b584bea54cc5eb7af
aaaKwinaaa <focus.p2012@hotmail.com>
2021-11-24 17:21:49 +0700

    add cat
    just add cat and suffle

--- assets/data/demo.ts
+++ assets/data/demo.ts
@@ -12,10 +12,10 @@ import IMAGE_10 from "../images/dog10.jpg";
 import IMAGE_13 from "../images/dog13.jpg";
 import IMAGE_14 from "../images/dog14.jpg";
 
-import IMAGE_15 from "../images/dog14.jpg";
-import IMAGE_16 from "../images/dog14.jpg";
-import IMAGE_17 from "../images/dog14.jpg";
-import IMAGE_18 from "../images/dog14.jpg";
+import IMAGE_15 from "../images/cat1.jpg";
+import IMAGE_16 from "../images/cat2.jpg";
+import IMAGE_17 from "../images/cat3.jpg";
+import IMAGE_18 from "../images/cat4.jpg";
 
 import User_Profile from "../images/User_Profile.jpg";
 
@@ -34,13 +34,14 @@ export const animals: Animal[] = [
   },
   {
     id: 2,
-    name: "ซาร่า",
-    type: "dog",
-    ownerId: 2,
+    name: "บีกิน",
+    type: "cat",
+    ownerId: 4,
     description:
-      "มาตามหารักแท้ที่ดูแลกันได้ เด็กหญิงวัยสองเดือน ร่าเริง แข็งแรงมากๆ ถ่ายพยาธิและทำวัคซีนรวมห้าโรคให้แล้วสองเข็ม",
-    message: "ได้ครับผม",
-    image: IMAGE_02,
+      "ตามหาผู้ใจบุญรับเลี้ยงน้องแมวเพศเมีย(ดวงตามีปัญหา) เผอิญเจอน้องในโพรงหญ้าที่ไซต์งานก่อสร้าง คือถ้าปล่อยไว้ตรงนั้นน้องไม่น่ารอด",
+    message:
+      "ตอนนี้น้องกำลังฉีดวัคซีนโควิดอยู่ครับ อาจจะมีอาการเพลียเล็กน้อย",
+    image: IMAGE_17,
     distance: 2
   },
   {
@@ -49,7 +50,7 @@ export const animals: Animal[] = [
     type: "dog",
     ownerId: 3,
     description:
-      "สปริทช์จากการไม่พร้อมเลี้ยง ต้องอยู่แต่ในคอกสี่เหลี่ยมแคบๆมานานเท่าใด ไม่เคยได้มีโอกาสออกมาวิ่งเล่น ไม่ได้วัคซีนมาสองปีแล้ว ทางเพจช่วยมากรูมมิ่ง ตรวจเลือด เตรียมทำหมันและตามหาเจ้านายใจดีที่พร้อมชดเชยให้ความรักความอบอุ่น  ",
+      "สปริทช์จากการไม่พร้อมเลี้ยง ต้องอยู่แต่ในคอกสี่เหลี่ยมแคบๆมานานเท่าใด ไม่เคยได้มีโอกาสออกมาวิ่งเล่น ไม่ได้วัคซีนมาสองปีแล้ว ทางเพจช่วยมากรูมมิ่ง ตรวจเลือด ",
     message:
       "น้องมีคนรับไปเลี้ยงแล้วค่ะ ขอโทษด้วยนะคะ",
     image: IMAGE_03,
@@ -68,27 +69,32 @@ export const animals: Animal[] = [
   },
   {
     id: 5,
-    name: "เต้าหู้",
-    type: "dog",
-    ownerId: 1,
-    distance: 5,
+    name: "บายู",
+    type: "cat",
+    ownerId: 4,
     description:
-      "เดินหลงจากบ้านมาไกลและนานจนหาทางกลับไม่ถูก ไม่มีใครตามหา รอแล้วรออีก วนเวียนอยู่ที่เดิมจนคนที่เจอสงสารเลยช่วยมาหาบ้าน",
-    message: "มึงสิ อิกะเทย!",
-    image: IMAGE_05,
-  },
-  {
-    id: 6,
-    name: "โตโน่",
-    type: "dog",
-    ownerId: 3,
-    description:
-      "เมือไรจะถึงคิวของโตโน่ บ้างค้าบ ในเมื่อทุกชีวิต​มีสิทธิ์​มีบ้าน​ บีเกิ้ลผสมตกยาก รอดชีวิตมาได้ ก็อยากมีชีวิตใหม่ที่สดใสและอบอุ่น",
+      "น้องเป็นเพศผู้ วัยกำลังโต อายุน่าจะประมาณ 7-8 เดือนครับ พี่ๆท่านใดเมตตาอยากรับน้องไปดูแล สามารถปัดมาได้ตลอดเลยนะครับ ไม่มีค่าใช้จ่ายใดๆครับ",
     message:
-      "Thx you krub ^^",
-    image: IMAGE_06,
-    distance: 0.3
+      "ขอบคุณมากๆนะครับ ที่สนใจ ว่าแต่น้องบ้านอยู่ไหนครับ",
+    image: IMAGE_15,
+    distance: 3
+
   },
+   
+ {
+  id: 6,
+  name: "โตโน่",
+  type: "dog",
+  ownerId: 3,
+  description:
+    "เมือไรจะถึงคิวของโตโน่ บ้างค้าบ ในเมื่อทุกชีวิต​มีสิทธิ์​มีบ้าน​ บีเกิ้ลผสมตกยาก รอดชีวิตมาได้ ก็อยากมีชีวิตใหม่ที่สดใสและอบอุ่น",
+  message:
+    "Thx you krub ^^",
+  image: IMAGE_06,
+  distance: 0.3
+ },
+    
+  
   {
     id: 7,
     name: "เหลง",
@@ -99,25 +105,18 @@ export const animals: Animal[] = [
     message:
       "งั้นยังไม่รับก่อนนะครับ ไกลไปหน่อย",
     image: IMAGE_07,
-    distance: 2
-  },
+    distance: 2.7
+  },  
   {
     id: 8,
-    name: "มิเชล",
-    type: "dog",
-    ownerId: 1,
-    distance: 5,
-    description:
-      "ทำยังไง​ให้มิเชล​  อายุขวากว่าได้มีบ้านอีกสักครั้ง​ ชีวิตเหมือนจะดี​ แต่เหมือนโลกทั้งใบดับสลาย ขอ​โอกาสให้หมาไทยตัวเล็กๆ​ได้มีชี​วิตใหม่ที​อบอุ่น​ปลอดภัย​ด้วยค่า",
-    age: "27",
-    location: "Irvine, CA",
-    info1: 'Straight, Single, 5"10',
-    info2: "Tea Totaller, Loves Photography & Travel",
-    info3: "Beaches, Mountain, Cafe, Movies",
-    info4: "Last seen: 23h ago",
+    name: "บงบง",
+    type: "cat",
+    ownerId: 4,
+    description: "แมวจรหาบ้านบางพลีสมุทรปราการน้องประสบอุบัติเหตุบนถนนจึงนำน้องไปรักษาตอนนี้น้องหายดีแล้วแต่มีแผลเป็นที่ขาหลัง น้องสามารถเดินและวิ่งเล่นได้ตามปกติ",
     message:
-      "รับงานไหมครับ",
-    image: IMAGE_08,
+      "น้องขี้เล่นมากเลยนะครับ",
+    image: IMAGE_16,
+    distance: 3
   },
   {
     id: 9,
@@ -143,6 +142,59 @@ export const animals: Animal[] = [
     image: IMAGE_14,
     distance: 1
   },
+  {
+    id: 11,
+    name: "เต้าหู้",
+    type: "dog",
+    ownerId: 1,
+    description:
+      "เดินหลงจากบ้านมาไกลและนานจนหาทางกลับไม่ถูก ไม่มีใครตามหา รอแล้วรออีก วนเวียนอยู่ที่เดิมจนคนที่เจอสงสารเลยช่วยมาหาบ้าน",
+    message: "มึงสิ อิกะเทย!",
+    image: IMAGE_05,
+    distance: 2
+  },
+  {
+    id: 12,
+    name: "มิเชล",
+    type: "dog",
+    ownerId: 1,
+    description:
+      "ทำยังไง​ให้มิเชล​  อายุขวากว่าได้มีบ้านอีกสักครั้ง​ ชีวิตเหมือนจะดี​ แต่เหมือนโลกทั้งใบดับสลาย ขอ​โอกาสให้หมาไทยตัวเล็กๆ​ได้มีชี​วิตใหม่ที​อบอุ่น​ปลอดภัย​ด้วยค่า",
+    age: "27",
+    location: "Irvine, CA",
+    info1: 'Straight, Single, 5"10',
+    info2: "Tea Totaller, Loves Photography & Travel",
+    info3: "Beaches, Mountain, Cafe, Movies",
+    info4: "Last seen: 23h ago",
+    message:
+      "รับงานไหมครับ",
+    image: IMAGE_08,
+    distance: 3
+  },
+  {
+    id: 13,
+    name: "ซาร่า",
+    type: "dog",
+    ownerId: 2,
+    description:
+      "มาตามหารักแท้ที่ดูแลกันได้ เด็กหญิงวัยสองเดือน ร่าเริง แข็งแรงมากๆ ถ่ายพยาธิและทำวัคซีนรวมห้าโรคให้แล้วสองเข็ม",
+    message: "ได้ครับผม",
+    image: IMAGE_02,
+    distance: 2
+  }, 
+  {
+    id: 14,
+    name: "บียอน",
+    type: "cat",
+    ownerId: 4,
+    description:
+      "หาบ้านให้น้องแมวเพศเมียอายุประมาณ 1 ปีทำหมันให้แล้วครับน้องเป็นแมวไม่มีเจ้าของครับ ได้แต่ขออาหารกินไปวันๆ พี่ๆท่านใดเมตตาอยากรับน้องไปอยู่ด้วย ปัดขวามาเลยครับ",
+    message:
+      "น้องบียอนสุดน่ารัก กินเยอะแต่ขี้อ้อนแน่นอน",
+    image: IMAGE_18,
+    distance: 1.5
+  },
+  
 ];
 
 export const owners: Owner[] = [
@@ -177,14 +229,14 @@ export const users: User[] = [
     id: 1,
     name: "Kwin Prapawong",
     description:
-      "nice nice nice",
+      "ขอแบบ nice nice nice อ่ะคร้าบบบบ",
     message:
       "",
     image: User_Profile,
     age: "22",
     location: "รัชดา, กรุงเทพ",
     info1: 'โปรแกรมเมอร์อิสระ ,โสด, 56',
-    info2: "รักสัตว์ อยู่คนเดียว อยากรับสัตว์มาอยู่เป็นเพื่อนครับ",
+    info2: "รักสัตว์ อยู่คนเดียว อยากรับน้องแงวมาอยู่เป็นเพื่อนครับ",
     adopt: 4,
 
   },
